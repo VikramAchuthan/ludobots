8ff50fd987f008f54b3795ec88d7fc53a734aff4
VikramAchuthan <vikram@achuthans.com>
2023-01-12 08:12:53 -0600

    part 1 of task f.

--- simulate.py
+++ simulate.py
@@ -1,5 +1,8 @@
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+
+import numpy
 import time
 
 physicsClient = p.connect(p.GUI)
@@ -8,11 +11,19 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
-p.loadSDF("boxes.sdf")
 
+p.loadSDF("world.sdf")
+
+
+pyrosim.Prepare_To_Simulate(robotId)
+
+frontLegSensorValues = mumpy.zeros(10000)
 for x in range(0,1000):
 	p.stepSimulation()
-	#time.sleep(1/30)
+	frontLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+	print(frontLegTouch)
+	time.sleep(1/30)
 p.disconnect()
 
