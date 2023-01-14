bd73dde6975150d88aaaa1c9de47771de3f4e62b
VikramAchuthan <vikram@achuthans.com>
2023-01-14 13:04:07 -0600

    finished task f analyze

--- analyze.py
+++ analyze.py
@@ -7,10 +7,17 @@ backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
 
 print(frontLegSensorValues)
 
-plt.plot(frontLegSensorValues, label = "Front Leg", linewidth = 5)
-plt.plot(backLegSensorValues, label = "Back Leg")
+# plt.plot(frontLegSensorValues, label = "Front Leg", linewidth = 5)
+# plt.plot(backLegSensorValues, label = "Back Leg")
+# plt.matplotlib.pyplot.legend()
+
+
+# 
+
+targetAngleValues_BackLeg = numpy.load("data/targetAngles_BackLeg.npy")
+targetAngleValues_FrontLeg = numpy.load("data/targetAngles_FrontLeg.npy")
+
+plt.plot(targetAngleValues_BackLeg, label = "BackLeg", linewidth = 5)
+plt.plot(targetAngleValues_FrontLeg, label = "Front Leg", linewidth = 1)
 plt.matplotlib.pyplot.legend()
-
-
 plt.matplotlib.pyplot.show()
-
